124670f87b729e6e5cb492ad6b5ec08ee0e2149f
Bryan Hansen <bryankhansen@gmail.com>
2025-06-29 21:34:20 -0600

    Replacing the content

--- src/main/java/com/pluralsight/beanconfig/util/PieCalculator.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight.beanconfig.util;
-
-public class PieCalculator {
-    public int calculateSlices(int diameter) {
-        return (int) Math.floor((Math.PI * diameter * diameter) / 14.0);
-    }
-}

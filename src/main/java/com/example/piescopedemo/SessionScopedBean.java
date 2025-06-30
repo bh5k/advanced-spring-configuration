e93be0c6159cc3e0630f57a41867891a9660d7ac
Bryan Hansen <bryankhansen@gmail.com>
2025-06-29 23:21:10 -0600

    Added scoped demo

---
+++ src/main/java/com/example/piescopedemo/SessionScopedBean.java
@@ -0,0 +1,17 @@
+package com.example.piescopedemo;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+@Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class SessionScopedBean {
+
+    private int visitCount = 0;
+
+    public int incrementVisit() {
+        return ++visitCount;
+    }
+}
